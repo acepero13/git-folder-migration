03500ad70e4994aad738846640e1cc9d250b72f7
amador <amador@moltomedia.de>
2018-09-28 16:15:55 +0200

    - Bugfix: was not excluding folders

--- src/configuration/folder_validator/excluded.py
+++ src/configuration/folder_validator/excluded.py
@@ -2,9 +2,9 @@ from configuration.folder_validator.decorator import Decorator
 
 
 class Excluded(Decorator):
-    def __init__(self, validator, allowed_folders):
+    def __init__(self, validator, not_allowed_folders):
         super().__init__(validator)
-        self.not_allowed_folders = allowed_folders
+        self.not_allowed_folders = not_allowed_folders
 
     def is_valid(self, folder_name):
         return self._validator.is_valid(folder_name) and folder_name not in self.not_allowed_folders
--- src/configuration/validator_factory.py
+++ src/configuration/validator_factory.py
@@ -14,7 +14,7 @@ class ValidatorFactory(object):
         if self.config.allowed_folders is not None and len(self.config.allowed_folders) > 0:
             validator = Included(validator, self.config.allowed_folders)
         if self.config.not_allowed_folders is not None and len(self.config.not_allowed_folders) > 0:
-            validator = Excluded(validator, self.config.allowed_folders)
+            validator = Excluded(validator, self.config.not_allowed_folders)
         if self.config.regex_for_folder_name is not None and len(self.config.regex_for_folder_name) > 0:
             validator = RegExp(validator, self.config.regex_for_folder_name)
         return validator
