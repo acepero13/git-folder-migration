f2321353904675fd535cffefc959c2b8bf312001
amador <amador@moltomedia.de>
2018-10-01 09:02:01 +0200

    - added config.dist - added gitignore file

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from io_utils.directory_discover import DirectoryDiscover
 from migrator.migrator import Migrator
 
 if __name__ == '__main__':
-    config = YamlParser('../resources/config.yaml').parse()
+    config = YamlParser('../resources/hydac.yaml').parse()
     dir_discover = DirectoryDiscover(config.folder_to_discover())
     migrator = Migrator(config, dir_discover)
     migrator.migrate()
